228fb8420820c2a4ec913c732f06ad4605afc28a
Prince Kumar <princekumark2004@gmail.com>
2025-04-26 00:06:23 +0530

    Update MobileMsg.jsx

--- src/Components/MobileMsg.jsx
+++ src/Components/MobileMsg.jsx
@@ -3,7 +3,7 @@ function MobileMsg(){
         <>
             <div className="bg-black h-screen w-full text-white flex items-center justify-center p-10">
                 <div className="bg-[#18181B] p-5 rounded-xl">
-                    <p className="text-lg text-center font-medium">This part is optimized for desktop devices only. Please open it on a desktop for the better experience. <span className="text-purple-600 text-sm">You can perform comparison on this device.</span></p>
+                    <p className="text-lg text-center font-medium">This feature is optimized for desktop devices only. Please open it on a desktop for the better experience. <span className="text-purple-600 text-sm">You can perform comparison on this device.</span></p>
                 </div>
             </div>
         </>
